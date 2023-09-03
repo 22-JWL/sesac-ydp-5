158ee1fcadee059a15dd71537c2ceade05ec4417
22-JWL <jck1004ee@gmail.com>
2023-09-03 18:29:16 +0900

    study:popup cookie

--- 18-session-cookie/app.js
+++ 18-session-cookie/app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8000;
+const path = require('path'); //path를 import한다
+
+
+
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); //폴더, 폴더경로 지정
+
 
 // 미들웨어 등록
 // app.use(cookieParser()); // 일반 쿠키
