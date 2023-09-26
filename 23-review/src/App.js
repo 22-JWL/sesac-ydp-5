3f6b6fbbd6a8f9f8f60e33b6be6dc547e792dc7e
22-JWL <jck1004ee@gmail.com>
2023-09-26 20:19:00 +0900

    study:react

---
+++ 23-review/src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { fragment } from 'react';
+
+function App() {
+  const name = '리액트';
+  const a = 10,
+    b = 6;
+
+  return (
+    <>
+      <div className="react">
+        이것은 div입니다. <br />
+        <h3>이것은 div 안에 있는 h3 태그 입니다.</h3>
+      </div>
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+
+      <h2>
+        제 반려동물의 이름은 <span className="bold">로이</span>입니다. <br />
+        <span style={(fontWeight = 'bold')}>로이</span>는{' '}
+        <span className="bold">강아지</span>입니다.
+      </h2>
+      {3 + 5 == 8 ? <>정답입니다</> : <>오답입니다.</>}
+      <br />
+
+      {(b > a && <>a가 b보다 큽니다</>) || <>b가 a보다 큽니다</>}
+
+      <h1>Hello World</h1>
+
+      {/* <div style={ backgrountdColor = }></div> */}
+    </>
+  );
+}
+
+export default App;
