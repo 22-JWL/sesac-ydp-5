fc99d28cf5acacbf37406bd9ce12f139602c87b1
22-JWL <jck1004ee@gmail.com>
2023-10-11 14:24:57 +0900

    study:Css

---
+++ 29-style/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CssModuleComponents from './CssModuleComponents.js';
+
+function App() {
+  return (
+    <div className="App">
+      <CssModuleComponents />
+    </div>
+  );
+}
+
+export default App;
